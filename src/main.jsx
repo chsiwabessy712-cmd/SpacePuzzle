601243fe8684feba418d7898780d2e9db3d71f0f
Christian Siwabessy <siwabessy712@gmail.com>
2026-07-01 15:38:55 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { polyfill } from "mobile-drag-drop";
+import { scrollBehaviourDragImageTranslateOverride } from "mobile-drag-drop/scroll-behaviour";
+import "mobile-drag-drop/default.css";
+
+const isMobileOrTabletView = window.matchMedia("(max-width: 1366px)").matches;
+polyfill({
+  dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride,
+  forceApply: isMobileOrTabletView
+});
+window.addEventListener('touchmove', function() {}, {passive: false});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
